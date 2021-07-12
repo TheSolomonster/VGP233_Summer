50b53ab8c8c7a87222a82706828af95bcfaf143d
aidan <aidan@LAPTOP-ROEVOHU3>
2021-07-12 05:21:33 +0100

    assignment 1 finished

---
+++ Aidan_Solomon_Assignment_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public float offsetXValue;
+    private Vector3 offset = new Vector3( 0.0f,0.0f,0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset.x = offsetXValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
